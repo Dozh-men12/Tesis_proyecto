1eb4cedaeed09a2b5e03562f791f623c6dbc5c34
Oscar <ohohurtado16@gmail.com>
2023-06-16 01:43:01 -0500

    Primer commit - Oscar (16/06/2023)

---
+++ Tesis/Tesis/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesis.View;
+using Xamarin.Forms;
+
+namespace Tesis
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
